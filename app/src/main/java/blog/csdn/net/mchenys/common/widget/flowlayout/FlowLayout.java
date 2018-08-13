f71ce6ae6dfedaa79965987d2f7ddda21216528c
mChenys <643353964@qq.com>
2018-08-13 09:28:08 +0800

    流式布局添加

---
+++ app/src/main/java/blog/csdn/net/mchenys/common/widget/flowlayout/FlowLayout.java
@@ -0,0 +1,227 @@
+package blog.csdn.net.mchenys.common.widget.flowlayout;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.v4.text.TextUtilsCompat;
+import android.util.AttributeSet;
+import android.util.LayoutDirection;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+import blog.csdn.net.mchenys.R;
+
+/**
+ * 流式布局  https://github.com/hongyangAndroid/FlowLayout
+ */
+public class FlowLayout extends ViewGroup {
+    private static final String TAG = "FlowLayout";
+    private static final int LEFT = -1;
+    private static final int CENTER = 0;
+    private static final int RIGHT = 1;
+
+    protected List<List<View>> mAllViews = new ArrayList<List<View>>();
+    protected List<Integer> mLineHeight = new ArrayList<Integer>();
+    protected List<Integer> mLineWidth = new ArrayList<Integer>();
+    private int mGravity;
+    private List<View> lineViews = new ArrayList<>();
+
+    public FlowLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.TagFlowLayout);
+        mGravity = ta.getInt(R.styleable.TagFlowLayout_tag_gravity, LEFT);
+        int layoutDirection = TextUtilsCompat.getLayoutDirectionFromLocale(Locale.getDefault());
+        if (layoutDirection == LayoutDirection.RTL) {
+            if (mGravity == LEFT) {
+                mGravity = RIGHT;
+            } else {
+                mGravity = LEFT;
+            }
+        }
+        ta.recycle();
+    }
+
+    public FlowLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public FlowLayout(Context context) {
+        this(context, null);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int sizeWidth = MeasureSpec.getSize(widthMeasureSpec);
+        int modeWidth = MeasureSpec.getMode(widthMeasureSpec);
+        int sizeHeight = MeasureSpec.getSize(heightMeasureSpec);
+        int modeHeight = MeasureSpec.getMode(heightMeasureSpec);
+
+        // wrap_content
+        int width = 0;
+        int height = 0;
+
+        int lineWidth = 0;
+        int lineHeight = 0;
+
+        int cCount = getChildCount();
+
+        for (int i = 0; i < cCount; i++) {
+            View child = getChildAt(i);
+            if (child.getVisibility() == View.GONE) {
+                if (i == cCount - 1) {
+                    width = Math.max(lineWidth, width);
+                    height += lineHeight;
+                }
+                continue;
+            }
+            measureChild(child, widthMeasureSpec, heightMeasureSpec);
+            MarginLayoutParams lp = (MarginLayoutParams) child
+                    .getLayoutParams();
+
+            int childWidth = child.getMeasuredWidth() + lp.leftMargin
+                    + lp.rightMargin;
+            int childHeight = child.getMeasuredHeight() + lp.topMargin
+                    + lp.bottomMargin;
+
+            if (lineWidth + childWidth > sizeWidth - getPaddingLeft() - getPaddingRight()) {
+                //需要换行
+                width = Math.max(width, lineWidth);
+                lineWidth = childWidth;
+                height += lineHeight;//累加包裹内容所需的高度
+                lineHeight = childHeight;
+            } else {
+                //不用换行
+                lineWidth += childWidth;
+                lineHeight = Math.max(lineHeight, childHeight);//取当前行最大高度作为行高
+            }
+            if (i == cCount - 1) {
+                //包裹内容所需的最大宽度
+                width = Math.max(lineWidth, width);
+                //累加最后一行的高度
+                height += lineHeight;
+            }
+        }
+        setMeasuredDimension(
+                //父控件宽高确定则用确定的,否则用测量后的
+                modeWidth == MeasureSpec.EXACTLY ? sizeWidth : width + getPaddingLeft() + getPaddingRight(),
+                modeHeight == MeasureSpec.EXACTLY ? sizeHeight : height + getPaddingTop() + getPaddingBottom()//
+        );
+
+    }
+
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        mAllViews.clear();//所有行的view
+        mLineHeight.clear();//每一行的高度
+        mLineWidth.clear();//每一行的宽度
+        lineViews.clear();//每一行的view
+
+        int width = getWidth();
+
+        int lineWidth = 0;
+        int lineHeight = 0;
+
+        int cCount = getChildCount();
+
+        for (int i = 0; i < cCount; i++) {
+            View child = getChildAt(i);
+            if (child.getVisibility() == View.GONE) continue;
+            MarginLayoutParams lp = (MarginLayoutParams) child
+                    .getLayoutParams();
+
+            int childWidth = child.getMeasuredWidth();
+            int childHeight = child.getMeasuredHeight();
+
+            if (childWidth + lineWidth + lp.leftMargin + lp.rightMargin > width - getPaddingLeft() - getPaddingRight()) {
+                //换行,保存数据
+                mLineHeight.add(lineHeight);
+                mAllViews.add(lineViews);
+                mLineWidth.add(lineWidth);
+
+                lineWidth = 0;
+                lineHeight = childHeight + lp.topMargin + lp.bottomMargin;
+                lineViews = new ArrayList<View>();
+            }
+            lineWidth += childWidth + lp.leftMargin + lp.rightMargin;
+            lineHeight = Math.max(lineHeight, childHeight + lp.topMargin
+                    + lp.bottomMargin);
+            lineViews.add(child);
+
+        }
+        mLineHeight.add(lineHeight);
+        mLineWidth.add(lineWidth);
+        mAllViews.add(lineViews);
+
+
+        int left = getPaddingLeft();
+        int top = getPaddingTop();
+
+        int lineNum = mAllViews.size();
+
+        for (int i = 0; i < lineNum; i++) {
+            lineViews = mAllViews.get(i);
+            lineHeight = mLineHeight.get(i);
+
+            // set gravity
+            int currentLineWidth = this.mLineWidth.get(i);
+            switch (this.mGravity) {
+                case LEFT:
+                    left = getPaddingLeft();
+                    break;
+                case CENTER:
+                    left = (width - currentLineWidth) / 2 + getPaddingLeft();
+                    break;
+                case RIGHT:
+                    //  适配了rtl，需要补偿一个padding值
+                    left = width - (currentLineWidth + getPaddingLeft()) - getPaddingRight();
+                    //  适配了rtl，需要把lineViews里面的数组倒序排,从右边开始存放view
+                    Collections.reverse(lineViews);
+                    break;
+            }
+
+            for (int j = 0; j < lineViews.size(); j++) {
+                View child = lineViews.get(j);
+                if (child.getVisibility() == View.GONE) {
+                    continue;
+                }
+
+                MarginLayoutParams lp = (MarginLayoutParams) child
+                        .getLayoutParams();
+
+                int lc = left + lp.leftMargin;
+                int tc = top + lp.topMargin;
+                int rc = lc + child.getMeasuredWidth();
+                int bc = tc + child.getMeasuredHeight();
+
+                child.layout(lc, tc, rc, bc);
+
+                //更新下一个view添加到当前行的left
+                left += child.getMeasuredWidth() + lp.leftMargin
+                        + lp.rightMargin;
+            }
+            //更新下一个view添加到下一行的top
+            top += lineHeight;
+        }
+
+    }
+
+    @Override
+    public LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new MarginLayoutParams(getContext(), attrs);
+    }
+
+    @Override
+    protected LayoutParams generateDefaultLayoutParams() {
+        return new MarginLayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+    }
+
+    @Override
+    protected LayoutParams generateLayoutParams(LayoutParams p) {
+        return new MarginLayoutParams(p);
+    }
+}
---
+++ app/src/main/java/blog/csdn/net/mchenys/common/widget/flowlayout/TagFlowLayout.java
@@ -0,0 +1,270 @@
+package blog.csdn.net.mchenys.common.widget.flowlayout;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import blog.csdn.net.mchenys.R;
+
+/**
+ * 用于布局TagView标签容器的流式布局
+ * Created by zhy on 15/9/10.
+ */
+public class TagFlowLayout extends FlowLayout
+        implements TagAdapter.OnDataChangedListener {
+
+    private TagAdapter mTagAdapter;
+    private int mSelectedMax = -1;//-1为不限制数量
+    private static final String TAG = "TagFlowLayout";
+
+    //所有已选择的TagView的位置
+    private Set<Integer> mSelectedView = new HashSet<Integer>();
+
+    private OnSelectListener mOnSelectListener;
+    private OnTagClickListener mOnTagClickListener;
+
+    public interface OnSelectListener {
+        void onSelected(Set<Integer> selectPosSet);
+    }
+
+    public interface OnTagClickListener {
+        boolean onTagClick(View view, int position, FlowLayout parent);
+    }
+
+    public TagFlowLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.TagFlowLayout);
+        mSelectedMax = ta.getInt(R.styleable.TagFlowLayout_max_select, -1);
+        ta.recycle();
+    }
+
+    public TagFlowLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public TagFlowLayout(Context context) {
+        this(context, null);
+    }
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int cCount = getChildCount();
+        for (int i = 0; i < cCount; i++) {
+            TagView tagView = (TagView) getChildAt(i);
+            if (tagView.getVisibility() == View.GONE) {
+                continue;
+            }
+            if (tagView.getTagView().getVisibility() == View.GONE) {
+                tagView.setVisibility(View.GONE);
+            }
+        }
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+
+    public void setOnSelectListener(OnSelectListener onSelectListener) {
+        mOnSelectListener = onSelectListener;
+    }
+
+
+    public void setOnTagClickListener(OnTagClickListener onTagClickListener) {
+        mOnTagClickListener = onTagClickListener;
+    }
+
+    public void setAdapter(TagAdapter adapter) {
+        mTagAdapter = adapter;
+        mTagAdapter.setOnDataChangedListener(this);
+        mSelectedView.clear();
+        changeAdapter();
+    }
+
+    @SuppressWarnings("ResourceType")
+    private void changeAdapter() {
+        removeAllViews();
+        TagAdapter adapter = mTagAdapter;
+        TagView tagViewContainer = null;//存放标签的tag容器
+        HashSet preCheckedList = mTagAdapter.getPreCheckedList();
+        for (int i = 0; i < adapter.getCount(); i++) {
+            //初始化自定义布局
+            View tagView = adapter.getView(this, i, adapter.getItem(i));
+            //创建存放标签的容器
+            tagViewContainer = new TagView(getContext());
+            //将父控件状态变化强加到其子控件中,这样就可以通过改变父控件的状态来改变其包裹的子View状态
+            tagView.setDuplicateParentStateEnabled(true);
+            if (tagView.getLayoutParams() != null) {
+                //将自定义的布局数据设置到tag容器中
+                tagViewContainer.setLayoutParams(tagView.getLayoutParams());
+            } else {
+                MarginLayoutParams lp = new MarginLayoutParams(
+                        LayoutParams.WRAP_CONTENT,
+                        LayoutParams.WRAP_CONTENT);
+                lp.setMargins(dip2px(getContext(), 5),
+                        dip2px(getContext(), 5),
+                        dip2px(getContext(), 5),
+                        dip2px(getContext(), 5));
+                tagViewContainer.setLayoutParams(lp);
+            }
+            LayoutParams lp = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+            tagView.setLayoutParams(lp);
+            //添加自定义的布局到tag容器中
+            tagViewContainer.addView(tagView);
+            //将容器添加到流式布局中
+            addView(tagViewContainer);
+
+            //设置预定义选中的view的状态
+            if (preCheckedList.contains(i)) {
+                setChildChecked(i, tagViewContainer);
+            }
+
+            //判断列表中那些需要动态设置状态的
+            if (mTagAdapter.setSelected(i, adapter.getItem(i))) {
+                setChildChecked(i, tagViewContainer);
+            }
+            //去掉指定布局的焦点
+            tagView.setClickable(false);
+            final TagView finalTagViewContainer = tagViewContainer;
+            final int position = i;
+            //tag容器的点击事件
+            tagViewContainer.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //处理选择
+                    doSelect(finalTagViewContainer, position);
+                    if (mOnTagClickListener != null) {
+                        mOnTagClickListener.onTagClick(finalTagViewContainer, position,
+                                TagFlowLayout.this);
+                    }
+                }
+            });
+        }
+        mSelectedView.addAll(preCheckedList);
+    }
+
+    public void setMaxSelectCount(int count) {
+        if (mSelectedView.size() > count) {
+            Log.w(TAG, "you has already select more than " + count + " views , so it will be clear .");
+            mSelectedView.clear();
+        }
+        mSelectedMax = count;
+    }
+
+    public Set<Integer> getSelectedList() {
+        return new HashSet<Integer>(mSelectedView);
+    }
+
+    private void setChildChecked(int position, TagView view) {
+        view.setChecked(true);
+        mTagAdapter.onSelected(position, view.getTagView());
+    }
+
+    private void setChildUnChecked(int position, TagView view) {
+        view.setChecked(false);
+        mTagAdapter.unSelected(position, view.getTagView());
+    }
+    //处理tag容器的点击事件,改变view的选中状态和记录选中的索引
+    private void doSelect(TagView child, int position) {
+        if (!child.isChecked()) {
+            //未选中->已选中
+            //处理max_select=1的情况,(单选)
+            if (mSelectedMax == 1 && mSelectedView.size() == 1) {
+                Iterator<Integer> iterator = mSelectedView.iterator();
+                Integer preIndex = iterator.next();
+                TagView pre = (TagView) getChildAt(preIndex);
+                setChildUnChecked(preIndex, pre);//重置上一次选择的TagView状态
+                setChildChecked(position, child);
+
+                mSelectedView.remove(preIndex);
+                mSelectedView.add(position);
+            } else {//多选
+                if (mSelectedMax > 0 && mSelectedView.size() >= mSelectedMax) {
+                    //超过最大数不能继续选择
+                    return;
+                }
+                //mSelectedMax=-1,不限个数(默认)
+                setChildChecked(position, child);
+                mSelectedView.add(position);
+            }
+        } else {
+            //已选中->未选中
+            setChildUnChecked(position, child);
+            mSelectedView.remove(position);
+        }
+        //实时通知选当前选中的view的index
+        if (mOnSelectListener != null) {
+            mOnSelectListener.onSelected(new HashSet<Integer>(mSelectedView));
+        }
+    }
+
+    public TagAdapter getAdapter() {
+        return mTagAdapter;
+    }
+
+
+    private static final String KEY_CHOOSE_POS = "key_choose_pos";
+    private static final String KEY_DEFAULT = "key_default";
+
+
+    @Override
+    protected Parcelable onSaveInstanceState() {
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(KEY_DEFAULT, super.onSaveInstanceState());
+
+        String selectPos = "";
+        if (mSelectedView.size() > 0) {
+            for (int key : mSelectedView) {
+                selectPos += key + "|";
+            }
+            selectPos = selectPos.substring(0, selectPos.length() - 1);
+        }
+        //保存存储的数据
+        bundle.putString(KEY_CHOOSE_POS, selectPos);
+        return bundle;
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Parcelable state) {
+        //恢复存储的数据
+        if (state instanceof Bundle) {
+            Bundle bundle = (Bundle) state;
+            String mSelectPos = bundle.getString(KEY_CHOOSE_POS);
+            if (!TextUtils.isEmpty(mSelectPos)) {
+                String[] split = mSelectPos.split("\\|");
+                for (String pos : split) {
+                    int index = Integer.parseInt(pos);
+                    mSelectedView.add(index);
+
+                    TagView tagView = (TagView) getChildAt(index);
+                    if (tagView != null) {
+                        setChildChecked(index, tagView);
+                    }
+                }
+
+            }
+            super.onRestoreInstanceState(bundle.getParcelable(KEY_DEFAULT));
+            return;
+        }
+        super.onRestoreInstanceState(state);
+    }
+
+
+    @Override
+    public void onChanged() {
+        mSelectedView.clear();
+        changeAdapter();
+    }
+
+    public static int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+}
